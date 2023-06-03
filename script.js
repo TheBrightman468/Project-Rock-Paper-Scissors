15130d4dd5a9a2d864eee9a174bc4bb5a18d1db8
Alex <122511865+TheBrightman468@users.noreply.github.com>
2023-06-03 17:21:30 +1000

    Create: Add function & variable to script.js
    I added a function getComputerChoice that takes the value of randomInt
    and converts it to one of three strings; "Rock", "Paper", or "Scissors".
    I did this using if/if else conditional syntax within the function call.
    The value stored in randomInt can only be a 1, 2, or 3, so I included a
    contitional test for each number, thereby transforming a random number
    generator into one of three random strings.
    
    I then added a variable computerChoice to store the return output of the
    getComputerChoice function.

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 let randomInt = getRandomInt();
+
+function getComputerChoice() {
+    if (randomInt === 1) {
+        return "Rock"
+    } else if (randomInt === 2) {
+        return "Paper"
+    } else if (randomInt === 3) {
+        return "Scissors"
+    } else {
+        return "Something wrong here.."
+    }
+}
+let computerChoice = getComputerChoice();
