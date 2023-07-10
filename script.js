cd7f8589e7f3402a7da5b6a2a8e770eaf8e2538c
Alex <122511865+TheBrightman468@users.noreply.github.com>
2023-07-10 14:00:09 +1000

    Change: Clean up code structure & edit return text
    I decided to clean up the code for this program by consolidating
    all function and variable declarations and function calls into a single
    function: 'game'
    
    This program previously declared/initialised variables and functions in
    global scope, and then called the game function to play four rounds.
    This is because I needed to initialise functions and variables in global
    scope, which would inadvertedly invoke the core functions and play a
    single round before calling 'game' to play the remaining rounds.
    
    This was a result of my initial troubleshooting workflow, so I decided
    to come back to this project with fresh eyes and clean up the code.
    
    I also slightly edited the return strings in the finalResult function in
    order to better differentiate the final result message from the single
    round result messages.

--- script.js
+++ script.js
@@ -1,101 +1,102 @@
-let randomInt = getRandomInt();
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Rock, Paper, or Scissors?");
-let roundResult = playRound(playerSelection, computerSelection);
-let playerWin = 0;
-let computerWin = 0;
-
-console.log(roundResult);
-keepScore();
-game();
-
-function getRandomInt(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(4);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getComputerChoice() {
-    if (randomInt === 1) {
-        return "ROCK"
-    } else if (randomInt === 2) {
-        return "PAPER"
-    } else if (randomInt === 3) {
-        return "SCISSORS"
-    } else {
-        return "Something wrong here.."
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toUpperCase() === computerSelection) {
-        return "It's a draw!"
-    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "PAPER") {
-        return "You lose! Paper beats rock"
-    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "SCISSORS") {
-        return "You win! Rock beats scissors"
-    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "SCISSORS") {
-        return "You lose! Scissors beats paper"
-    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "ROCK") {
-        return "You win! Paper beats rock"
-    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "ROCK") {
-        return "You lose! Rock beats scissors"
-    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "PAPER") {
-        return "You win! Scissors beats paper"
-    } else {
-        return "Something wrong here.."
-    }
-}
-
-function keepScore() { 
-    if (roundResult === "It's a draw!") {
-        ++playerWin;
-        ++computerWin;
-    } else if (roundResult === "You win! Rock beats scissors" || roundResult === "You win! Paper beats rock" || roundResult === "You win! Scissors beats paper") {
-        ++playerWin;
-    } else if (roundResult === "You lose! Paper beats rock" || roundResult === "You lose! Scissors beats paper" || roundResult === "You lose! Rock beats scissors") {
-        ++computerWin;
-    }
-}
-
-function finalResult() {
-    if (playerWin === computerWin) {
-        return `It's a draw!! ${playerWin} - ${computerWin}`
-    } else if (playerWin > computerWin) {
-        return `You win!! ${playerWin} - ${computerWin}`
-    } else if (playerWin < computerWin) {
-        return `You lose!! ${playerWin} - ${computerWin}`
-    }
-}
-
 function game() {
-    randomInt = getRandomInt();
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Rock, Paper, or Scissors?");
-    roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult);
-    keepScore();
+    let playerWin = 0;
+    let computerWin = 0;
 
-    randomInt = getRandomInt();
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Rock, Paper, or Scissors?");
-    roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult);
-    keepScore();
+    let randomInt = getRandomInt();
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        keepScore();
 
-    randomInt = getRandomInt();
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Rock, Paper, or Scissors?");
-    roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult);
-    keepScore();
+        randomInt = getRandomInt();
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+        roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        keepScore();
 
-    randomInt = getRandomInt();
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Rock, Paper, or Scissors?");
-    roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult);
-    keepScore();
+        randomInt = getRandomInt();
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+        roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        keepScore();
 
-    console.log(finalResult());
+        randomInt = getRandomInt();
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+        roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        keepScore();
+
+        randomInt = getRandomInt();
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+        roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        keepScore();
+
+        console.log(finalResult());
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(1);
+        max = Math.floor(4);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function getComputerChoice() {
+        if (randomInt === 1) {
+            return "ROCK"
+        } else if (randomInt === 2) {
+            return "PAPER"
+        } else if (randomInt === 3) {
+            return "SCISSORS"
+        } else {
+            return "Something wrong here.."
+        }
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection.toUpperCase() === computerSelection) {
+            return "It's a draw!"
+        } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "PAPER") {
+            return "You lose! Paper beats rock"
+        } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "SCISSORS") {
+            return "You win! Rock beats scissors"
+        } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "SCISSORS") {
+            return "You lose! Scissors beats paper"
+        } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "ROCK") {
+            return "You win! Paper beats rock"
+        } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "ROCK") {
+            return "You lose! Rock beats scissors"
+        } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "PAPER") {
+            return "You win! Scissors beats paper"
+        } else {
+            return "Something wrong here.."
+        }
+    }
+
+    function keepScore() { 
+        if (roundResult === "It's a draw!") {
+            ++playerWin;
+            ++computerWin;
+        } else if (roundResult === "You win! Rock beats scissors" || roundResult === "You win! Paper beats rock" || roundResult === "You win! Scissors beats paper") {
+            ++playerWin;
+        } else if (roundResult === "You lose! Paper beats rock" || roundResult === "You lose! Scissors beats paper" || roundResult === "You lose! Rock beats scissors") {
+            ++computerWin;
+        }
+    }
+
+    function finalResult() {
+        if (playerWin === computerWin) {
+            return `The game is a draw!! ${playerWin} - ${computerWin}`
+        } else if (playerWin > computerWin) {
+            return `You won the game!! ${playerWin} - ${computerWin}`
+        } else if (playerWin < computerWin) {
+            return `You lost the game!! ${playerWin} - ${computerWin}`
+        }
+    }  
 }
+
+game();
